aa16254c515fe134ca6c7ad7d9653c3ab6c39d42
Ryan <rcarnovsky@gmail.com>
2018-04-11 13:17:12 -0400

    Create base layout

---
+++ GCEC-LaptopSIO-iOS/Controller/ScanViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ScanViewController.swift
+//  GCEC-LaptopSIO-iOS
+//
+//  Created by Ryan Carnovsky on 4/11/18.
+//  Copyright © 2018 Ryan Carnovsky. All rights reserved.
+//
+
+import UIKit
+
+class ScanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
