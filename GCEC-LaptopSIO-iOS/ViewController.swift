352195819ed3d42c80af695dc71dc06aff3634fe
Ryan <rcarnovsky@gmail.com>
2018-04-11 13:02:37 -0400

    Initial Commit

---
+++ GCEC-LaptopSIO-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GCEC-LaptopSIO-iOS
+//
+//  Created by Ryan Carnovsky on 4/11/18.
+//  Copyright © 2018 Ryan Carnovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
